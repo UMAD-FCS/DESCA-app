ad80c9fedce3303c953e518e8d586f63e4d471e9
martinopertti <operttim@gmail.com>
2021-11-26 13:03:22 -0300

    todas las notas conceptuales

--- indicadores/Fichas tecnicas/params.R
+++ indicadores/Fichas tecnicas/params.R
@@ -45,4 +45,199 @@ for (i in seq_along(filenames)) {
 }
 
 
+## Internacional - Poblaciones  ----
+
+base <- readxl::read_excel("Base Normativa Completa.xlsx") %>%
+  filter(Población != "Todos") 
+
+ruta <- "notas-pdf/internacional/poblaciones/"
+
+lista_id <- base$id
+
+## Loop para crear markdown en html
+
+for (i in lista_id) {
+  
+  out <- paste0(ruta, "int-pob", i, ".html") 
+  
+  rmarkdown::render("Internacional-Poblaciones.Rmd", "html_document", output_file = out, 
+                    
+                    params = list(lista_id = i), encoding = "UTF-8")
+  
+}
+
+## Loop para pasar markdowns a pdf
+
+# Leer todas los documentos
+filenames <- list.files("notas-pdf/internacional/poblaciones", 
+                        pattern = "*.html",
+                        full.names=TRUE) 
+
+namesfiles <- substr(filenames, 34, 150) # Los modifico por comodidad
+namesfiles <- gsub(".html", "", namesfiles)
+
+# Cargo todas las bases con la función "read_twitter_csv" de rtweet 
+for (i in seq_along(filenames)) {
+  
+  chrome_print(filenames[i])
+  
+}
+
+
+## Constitucion - Derechos ----
+
+base <- readxl::read_excel("Base Normativa Completa.xlsx", 2) %>%
+  filter(Derecho != "Todos") 
+
+ruta <- "notas-pdf/constitucion/derechos/"
+
+lista_id <- base$id
+
+## Loop para crear markdown en html
+
+for (i in lista_id) {
+  
+  out <- paste0(ruta, "con-der", i, ".html") 
+  
+  rmarkdown::render("Constitucion-Derechos.Rmd", "html_document", output_file = out, 
+                    
+                    params = list(lista_id = i), encoding = "UTF-8")
+  
+}
+
+## Loop para pasar markdowns a pdf
+
+# Leer todas los documentos
+filenames <- list.files(ruta, 
+                        pattern = "*.html",
+                        full.names=TRUE) 
+
+namesfiles <- substr(filenames, 34, 150) # Los modifico por comodidad
+namesfiles <- gsub(".html", "", namesfiles)
+
+# Cargo todas las bases con la función "read_twitter_csv" de rtweet 
+for (i in seq_along(filenames)) {
+  
+  chrome_print(filenames[i])
+  
+}
+
+
+
+## Constitucion - Poblaciones  ----
+
+base <- readxl::read_excel("Base Normativa Completa.xlsx", 2) %>%
+  filter(Población != "Todos") 
+
+ruta <- "notas-pdf/constitucion/poblaciones/"
+
+lista_id <- base$id
+
+## Loop para crear markdown en html
+
+for (i in lista_id) {
+  
+  out <- paste0(ruta, "con-pob", i, ".html") 
+  
+  rmarkdown::render("Constitucion-Poblaciones.Rmd", "html_document", output_file = out, 
+                    
+                    params = list(lista_id = i), encoding = "UTF-8")
+  
+}
+
+## Loop para pasar markdowns a pdf
+
+# Leer todas los documentos
+filenames <- list.files(ruta, 
+                        pattern = "*.html",
+                        full.names=TRUE) 
+
+namesfiles <- substr(filenames, 34, 150) # Los modifico por comodidad
+namesfiles <- gsub(".html", "", namesfiles)
+
+# Cargo todas las bases con la función "read_twitter_csv" de rtweet 
+for (i in seq_along(filenames)) {
+  
+  chrome_print(filenames[i])
+  
+}
+
+
+## Leyes - Derechos ----
+
+base <- readxl::read_excel("Base Normativa Completa.xlsx", 3) %>%
+  filter(Derecho != "Todos") 
+
+ruta <- "notas-pdf/leyes/derechos/"
+
+lista_id <- base$id
+
+## Loop para crear markdown en html
+
+for (i in lista_id) {
+  
+  out <- paste0(ruta, "ley-der", i, ".html") 
+  
+  rmarkdown::render("Leyes-Derechos.Rmd", "html_document", output_file = out, 
+                    
+                    params = list(lista_id = i), encoding = "UTF-8")
+  
+}
+
+## Loop para pasar markdowns a pdf
+
+# Leer todas los documentos
+filenames <- list.files(ruta, 
+                        pattern = "*.html",
+                        full.names=TRUE) 
+
+namesfiles <- substr(filenames, 34, 150) # Los modifico por comodidad
+namesfiles <- gsub(".html", "", namesfiles)
+
+# Cargo todas las bases con la función "read_twitter_csv" de rtweet 
+for (i in seq_along(filenames)) {
+  
+  chrome_print(filenames[i])
+  
+}
+
+
+
+## Leyes - Poblaciones ----
+
+base <- readxl::read_excel("Base Normativa Completa.xlsx", 3) %>%
+  filter(Población != "Todos") 
+
+ruta <- "notas-pdf/leyes/poblaciones/"
+
+lista_id <- base$id
+
+## Loop para crear markdown en html
+
+for (i in lista_id) {
+  
+  out <- paste0(ruta, "ley-pob", i, ".html") 
+  
+  rmarkdown::render("Leyes-Poblaciones.Rmd", "html_document", output_file = out, 
+                    
+                    params = list(lista_id = i), encoding = "UTF-8")
+  
+}
+
+## Loop para pasar markdowns a pdf
+
+# Leer todas los documentos
+filenames <- list.files(ruta, 
+                        pattern = "*.html",
+                        full.names=TRUE) 
+
+namesfiles <- substr(filenames, 34, 150) # Los modifico por comodidad
+namesfiles <- gsub(".html", "", namesfiles)
+
+# Cargo todas las bases con la función "read_twitter_csv" de rtweet 
+for (i in seq_along(filenames)) {
+  
+  chrome_print(filenames[i])
+  
+}
 
